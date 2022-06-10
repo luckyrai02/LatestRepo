1311438db2142886b161dff8179377ce6174e4af
Upendra Rai <46902453+luckyrai02@users.noreply.github.com>
2022-06-11 01:02:29 +0530

    Add files via upload

---
+++ SpringblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucky.rai.springblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
